38c5de8b6739127ba49820353fe0b41b3bf072ec
Rafael <rafaelariovaldo02@gmail.com>
2021-12-30 23:24:04 -0300

    Exercicios Js.

--- Projetos/JavaScript/Exercicios.js
+++ Projetos/JavaScript/Exercicios.js
@@ -1,8 +1,46 @@
-{
+//{
     let weight;
-    console.log(typeof weight);
-}
- {
-        let na
+    weight = 'oi';
+   // console.log(typeof weight);
+//}
+//{
+   // let name = "Rafa";
+   // let number = 28;
+   // let stars =  2.500;
+    //let isSubcribed = true;
+//}
 
- }
+let student = 
+{
+     name : "Rafa",
+     age : 28,
+     wigth : 55,
+     stars :  2.500,
+     isSubcribed : true,
+}; // é um objeto
+     const Cuca = 
+     {
+        name: "cabeludo",
+        age: 24,
+        peso: 59,
+        ISsubscribe: true,
+     }
+    
+
+/*console.log(student.name);
+console.log("de idade tem :");
+console.log(student.age + "anos.");
+console.log("e pesa:")
+console.log(student.wigth + "kg.");*/
+
+// console.log(`${student.name} de idade ${student.age} anos e
+// pesa ${student.wigth} kg.`);
+
+let students = [
+    student,
+    Cuca,  
+];
+//console.log(students[0]);
+// console.log(students[1]);
+console.log(a)
+var a = 1;
