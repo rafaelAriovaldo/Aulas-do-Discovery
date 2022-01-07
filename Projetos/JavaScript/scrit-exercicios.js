167f9a9dec8706082fe92cf6c72dad8e0f46aea5
Rafael <rafaelariovaldo02@gmail.com>
2022-01-07 19:21:26 -0300

    exicicios concluidos.

---
+++ Projetos/JavaScript/scrit-exercicios.js
@@ -0,0 +1,28 @@
+var n = 60;
+
+if (n >= 90 && n >= 100) {
+    console.log("Note A")
+} else {
+    //console.log("erro de condição")
+}
+if (n <= 89 && n >= 80 ) {
+    console.log("Note B")
+} else {
+    //console.log("erro de condição 1")
+}
+if (n <= 79 && n >=70) {
+    console.log("Note C")
+} else {
+//console.log("erro de condição 2")    
+}
+if (n <= 69 && n >= 60) {
+    console.log("Note D")
+} else {
+    //console.log("erro de condição 3")
+}
+if (n < 60 && n >= 0 ) {
+    console.log("Se fudeu")
+    console.log("Cuidado nota vermelha")
+} else {
+//console.log("Cuidado nota vermelha")    
+}
