d85037ff8f11dae251bcaff41a2e382410fb72ce
Rafael <rafaelariovaldo02@gmail.com>
2022-01-10 15:00:08 -0300

    up-code-if-validedate.

--- Projetos/JavaScript/scrit-exercicios.js
+++ Projetos/JavaScript/scrit-exercicios.js
@@ -1,28 +1,46 @@
-var n = 60;
+var n = 105;
 
 if (n >= 90 && n <= 100) {
-    console.log("Note A")
+    console.log("Note A.")
+    console.log(n +".")
+    console.log("Parabêns, acerto miseravi.")
 } else {
     //console.log("erro de condição")
 }
 if (n <= 89 && n >= 80 ) {
     console.log("Note B")
+    console.log(n + ".")
+    console.log("Entendo.")
 } else {
     //console.log("erro de condição 1")
 }
 if (n <= 79 && n >=70) {
     console.log("Note C")
+    console.log(n + ".")
+    console.log("que dia foi isso.")
 } else {
 //console.log("erro de condição 2")    
 }
 if (n <= 69 && n >= 60) {
     console.log("Note D")
+    console.log(n + ".")
+    console.log("num sei.")
 } else {
     //console.log("erro de condição 3")
 }
 if (n < 60 && n >= 0 ) {
-    console.log("Se fudeu")
+    console.log("F")
     console.log("Cuidado nota vermelha")
 } else {
 //console.log("Cuidado nota vermelha")    
+}
+if (n >= 101) {
+    console.log("nota invalida.")
+} else {
+    
+}
+if (n < 0) {
+    console.log("nota invalida.")
+} else {
+    
 }
