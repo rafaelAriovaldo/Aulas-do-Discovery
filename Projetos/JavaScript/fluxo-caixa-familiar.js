631266036bbbab47fcd375581c41cae2677b57bf
Rafael <rafaelariovaldo02@gmail.com>
2022-01-11 00:57:05 -0300

    up-code-me-progresso.

--- Projetos/JavaScript/fluxo-caixa-familiar.js
+++ Projetos/JavaScript/fluxo-caixa-familiar.js
@@ -1,9 +1,31 @@
-let contas = {
-agua: 175.00,
-luz: 200.00,
-internet: 75.00,
-aluguel: 400.00,
-outros: 500.00, 
+let  despesasSaldo;
+let receitasSaldo;
+let saldoFinal;
+let agua = 150.00
+let luz = 175.00
+let salario = 1100
+let rendaExtra = 300
+function getSaldo(saldoFinal){
+    let despesas = {
+        agua,
+        luz,
+        }
+        let receitas = {
+            salario,
+            rendaExtra,
+        }
+      despesasSaldo = despesas.agua + despesas.luz
+      receitasSaldo = receitas.salario + receitas.rendaExtra
+      saldoFinal = receitasSaldo - despesasSaldo
+      
+
+if (saldoFinal > 0) {
+    console.log("saldo positivo")
+    console.log("R$:" + saldoFinal)
+} else {
+    console.log("saldo negativo")
+    console.log("R$:" + saldoFinal)
 }
-let recitas = []
-let despesas = []
+}
+getSaldo()
+// Fazer soma em arrays.
