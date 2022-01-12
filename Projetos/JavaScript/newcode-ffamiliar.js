cdd31fd24404d97cc6d1ecd6cd715c40b1289101
Rafael <rafaelariovaldo02@gmail.com>
2022-01-12 00:17:35 -0300

    Upcode ff em inicio.

--- Projetos/JavaScript/newcode-ffamiliar.js
+++ Projetos/JavaScript/newcode-ffamiliar.js
@@ -1,7 +1,12 @@
+let receitaTotal;
+let despesasTotal;
 let familia = {
 receitas: [100, 250, 300, 400],
 despesas: [50, 233, 25.5, 63.4],
+}
 
+function saldoFamiliar (){
+     receitaTotal = familia.receitas[0] + familia.receitas[1] +
+     familia.receitas[2] + familia.receitas[3];
 
 }
-console.log(receitas[0])
