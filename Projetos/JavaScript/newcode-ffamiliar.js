43dd81b97a3938d3b793deffebf87da0c0be84c9
Rafael <rafaelariovaldo02@gmail.com>
2022-01-12 16:14:22 -0300

    code-Up ff-caixa

--- Projetos/JavaScript/newcode-ffamiliar.js
+++ Projetos/JavaScript/newcode-ffamiliar.js
@@ -1,12 +1,30 @@
-let receitaTotal;
-let despesasTotal;
+
 let familia = {
 receitas: [100, 250, 300, 400],
 despesas: [50, 233, 25.5, 63.4],
 }
 
-function saldoFamiliar (){
-     receitaTotal = familia.receitas[0] + familia.receitas[1] +
-     familia.receitas[2] + familia.receitas[3];
-
+function soma(array){
+let total = 0;
+for(let value of array) {
+     total += value
 }
+}
+
+function saldoFamiliar (){
+  const receitaTotal = soma(familia.receitas)
+   const despesasTotal = soma(familia.despesas)
+
+   const total = receitaTotal - despesasTotal
+ 
+ const itOk = total >= 0
+ let  balanceText = "negativo"
+ 
+if (itOk) {
+
+      balanceText = "positivo"
+}
+console.log(`seu saldo é: ${balanceText} : ${total}`)
+}
+
+saldoFamiliar()
