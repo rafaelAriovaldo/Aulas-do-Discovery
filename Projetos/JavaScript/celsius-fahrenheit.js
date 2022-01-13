3e6ab79e1edd86b43f253c6e5f27d68a94dfd109
Rafael <rafaelariovaldo02@gmail.com>
2022-01-13 16:04:54 -0300

    up code conversion C - F.

---
+++ Projetos/JavaScript/celsius-fahrenheit.js
@@ -0,0 +1,17 @@
+
+function selector(C, F) {
+    C = "celsius";
+    F = "farenhite";
+if(C == "celsius"){
+   console.log (conversion.c)
+}
+    function conversion(ready,c,f) {
+
+         c = (ready - 32) * 5 / 9;
+
+         f = ready * 9 / 5 + 32;
+
+
+        return ready;
+    }
+}
