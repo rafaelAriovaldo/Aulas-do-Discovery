b14f3f8918cd6e898ac27fa634c6f0ef90134581
Rafael <rafaelariovaldo02@gmail.com>
2022-01-13 16:47:39 -0300

    Up-code C em F.

--- Projetos/JavaScript/celsius-fahrenheit.js
+++ Projetos/JavaScript/celsius-fahrenheit.js
@@ -1,17 +1,20 @@
 
-function selector(C, F) {
-    C = "celsius";
-    F = "farenhite";
-if(C == "celsius"){
-   console.log (conversion.c)
+
+
+function conversion(F) {
+
+    let C = "celsius";
+    let c = (F - 32) * 5/9
+    console.log(c + " " + C + ".")
+    return F;
+}   
+
+function conversion1(C){
+    let F = "fahenheit"
+    let f = C * 9/5 + 32
+    console.log(f + " " + F + ".")
+    return C;
 }
-    function conversion(ready,c,f) {
-
-         c = (ready - 32) * 5 / 9;
-
-         f = ready * 9 / 5 + 32;
 
 
-        return ready;
-    }
-}
+conversion(50)
