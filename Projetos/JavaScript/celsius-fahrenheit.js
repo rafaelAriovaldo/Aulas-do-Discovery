8648593ee48c215f2d343b6513eb7c1e1c17b2f9
Rafael <rafaelariovaldo02@gmail.com>
2022-01-16 16:36:35 -0300

    up-code C F

--- Projetos/JavaScript/celsius-fahrenheit.js
+++ Projetos/JavaScript/celsius-fahrenheit.js
@@ -1,18 +1,20 @@
 
 
 
-function conversion(n, C, F, ) {
-
+function conversion(n, C, F, V ) {
+    if(V == F){
     C = "celsius";
     
     let c = (n - 32) * 5 / 9
     console.log(c + " " + C + ".")
-    
+    }
+    else if (V == C){
     F = "fahenheit"
     let f = n * 9 / 5 + 32
     console.log(f + " " + F + ".")
+    }
     return n;
     
 }
 
-conversion(50)
+conversion(50,"celsius")
