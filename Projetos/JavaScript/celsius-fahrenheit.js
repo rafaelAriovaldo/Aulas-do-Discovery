442bcc701492f2020e1f885a4eaad434ebe55c72
Rafael <rafaelariovaldo02@gmail.com>
2022-01-17 22:07:52 -0300

    up code C e F final.

--- Projetos/JavaScript/celsius-fahrenheit.js
+++ Projetos/JavaScript/celsius-fahrenheit.js
@@ -1,20 +1,43 @@
 
+function getTempera(temp) {
+    const celsius = temp.toUpperCase().includes('C')
+    const farennite = temp.toUpperCase().includes('F')
 
+    if (!celsius && !farennite) {
+        throw new Error('grau não identificado')
+    }
+    let updateTemp = Number(temp.toUpperCase().replace("F", ""));
+    let formula = farennite => (farennite - 32) * 5 / 9
+    let tempSingn = 'C'
+    if (celsius) {
+        updateTemp = Number(temp.toUpperCase().replace("C", ""));
+        formula = celsius => (celsius * 9) * 5 + 32
+        tempSingn = 'F'
+    }
 
-function conversion(n, C, F, V ) {
-    if(V == F){
-    C = "celsius";
-    
-    let c = (n - 32) * 5 / 9
-    console.log(c + " " + C + ".")
-    }
-    else if (V == C){
-    F = "fahenheit"
-    let f = n * 9 / 5 + 32
-    console.log(f + " " + F + ".")
-    }
-    return n;
-    
+    return formula(updateTemp) + tempSingn;
+
 }
 
-conversion(50,"celsius")
+try {
+    console.log(getTempera('50C'))
+    console.log(getTempera('50F'))
+    console.log(getTempera('50Z'))
+} catch (error) {
+    console.log(error.message)
+}
+
+
+
+
+/*
+C = "celsius"
+let c = (n - 32) * 5 / 9
+console.log(c + " " + C + ".")
+F = "Farenit"
+let f = n * 9 / 5 + 32
+console.log(f + " " + F + ".")
+
+*/
+
+
