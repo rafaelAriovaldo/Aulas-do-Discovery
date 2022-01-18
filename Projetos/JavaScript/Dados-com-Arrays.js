32c4e58318731aa31ed179e53f7fc474386e6faf
Rafael <rafaelariovaldo02@gmail.com>
2022-01-17 22:34:10 -0300

    Up code Array.

---
+++ Projetos/JavaScript/Dados-com-Arrays.js
@@ -0,0 +1,38 @@
+const booksByCategory = [
+    {
+        category = "Riqueza",
+        books =[{
+            title: "Os segredos da mente milionaria",
+            author: "T.Harv Eker",
+        },
+        {
+            title: "O homem mais ric da babilônia",
+            author: "George S. Clarson",
+        },
+        {
+            title: "Pai rico, pai pobre",
+            author: "Robert T. Kyosaki e Sharon L. Lechter",
+        },
+
+        ],
+    },
+    {
+        category: "Inteleigencia emocional",
+        books: [{
+            title: "Você é insubistuível",
+            author: "Agusto Cury",
+        },
+        {
+            title: "Ansiedade - Como enfrentar o mal do Século",
+            author: "August Cury"
+        },
+        {
+            title: "Os 7 habitos das pessoas altamente eficases",
+            author: "Sptephen R. Covey",
+        },
+
+        ],
+    },
+
+];
+
