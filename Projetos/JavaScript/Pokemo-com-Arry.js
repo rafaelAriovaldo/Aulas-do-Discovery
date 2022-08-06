343f306543acc9221f11b9e006acb9328f2f9d8c
RafaelAriovaldo <rafaelariovaldo02@gmail.com>
2022-08-06 14:24:44 -0300

    Up code.

--- Projetos/JavaScript/Pokemo-com-Arry.js
+++ Projetos/JavaScript/Pokemo-com-Arry.js
@@ -15,9 +15,43 @@ const Pokemon = [
             Evolução: "Meganium",
         }],
         Pokedex: [{
-        IndexPokedex:154,
-        Tipo:"Planta",
-        Name:"Meganium",
+            IndexPokedex: 154,
+            Tipo: "Planta",
+            Name: "Meganium",
         }],
+        Pokedex: [{
+            IndexPokedex: 155,
+            Tipo: "Fogo",
+            Name: "Cyndaquil",
+            Evolução: "Quilava",
+        }],
+        Pokedex: [{
+            IndexPokedex: 156,
+            Tipo: "Fogo",
+            Name: "Quilava",
+        }],
+        Pokedex:[{
+        IndexPokedex:157,
+        Tipo:"Fogo",
+        Name:"Typloison",
+        }],
+        Pokedex: [{
+        IndexPokedex:158,
+        Tipo:"Agua",
+        Name:"Totodile",
+        Evolução:"Croconaw",
+        }],
+        Pokedex:[{
+            
+        IndexPokedex:159,
+        Tipo:"Agua",
+        Name:"Croconaw",
+        Evolução:"Feraligatr"
+        }],
+        Pokedex:[{
+        IndexPokedex:160,
+        Tipo:"Agua",
+        name:"Feraligatr"
+        }]
     }]
 
