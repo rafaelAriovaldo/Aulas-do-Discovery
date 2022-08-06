6e2d68da6d387ea8b095dfa08955850970e35198
RafaelAriovaldo <rafaelariovaldo02@gmail.com>
2022-08-06 13:59:12 -0300

    Pokemon com Arrays.

---
+++ Projetos/JavaScript/Pokemo-com-Arry.js
@@ -0,0 +1,23 @@
+const Pokemon = [
+    {
+
+        Pokedex: [{
+            IndexPokedex: 152,
+            Tipo: "Planta",
+            Name: "Chicorita",
+            Evolução: "Bayleef",
+        },
+        ],
+        Pokedex: [{
+            IndexPokedex: 153,
+            Tipo: "Planta",
+            Name: "Bayleef",
+            Evolução: "Meganium",
+        }],
+        Pokedex: [{
+        IndexPokedex:154,
+        Tipo:"Planta",
+        Name:"Meganium",
+        }],
+    }]
+
